23f1bbbc6caa5d66b36d46053715b5818be22ad4
Jan Kammerath <jankammerath@gmail.com>
2025-04-07 13:20:28 +0200

    Initial Commit

---
+++ MacLinuxKit/MacLinuxKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLinuxKitApp.swift
+//  MacLinuxKit
+//
+//  Created by Jan Kammerath on 07.04.25.
+//
+
+import SwiftUI
+
+@main
+struct MacLinuxKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
